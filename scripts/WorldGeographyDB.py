380ef2387c657d952050e5b7f887efa43e726643
ZenithClown <debmalyapramanik.005@gmail.com>
2023-09-02 22:04:55 +0530

    🎉 extract and transform data as per db schema

--- scripts/WorldGeographyDB.py
+++ scripts/WorldGeographyDB.py
@@ -15,14 +15,23 @@ be very easily replicated.
 """
 
 import os
-
+import uuid
 import pandas as pd
 
 if __name__ == "__main__":
     ROOT = ".." # project source directory
     DATA = os.path.join(ROOT, "sources", "WorldGeographyDB", "csv")
 
+    # ? Countries Master Table
     countries = pd.read_csv(os.path.join(DATA, "countries.csv"))
-    countries["CountryID"] = countries[["iso2", "iso3", "numeric_code"]].apply(
-        lambda x : f"{x[0]}-{x[1]}-{str(x[2]).zfill(3)}", axis = 1
-    )
+    countries = countries[["iso3", "name", "iso2", "region", "subregion", "capital", "currency", "tld", "latitude", "longitude"]]
+    countries.columns = ["CountryID", "CountryName", "ISO2", "RegionName", "SubRegionName", "CapitalName", "CurrencyCode", "TLD", "CountryLatitude", "CountryLongitude"]
+
+    # ? States Master Table
+    states = pd.read_csv("../sources/WorldGeographyDB/csv/states.csv")
+    states = states.merge(countries[["CountryID", "ISO2"]], left_on = "country_code", right_on = "ISO2")
+    states["StateID"] = [str(uuid.uuid4()).upper() for _ in range(states.shape[0])]
+    states = states[["StateID", "name", "CountryID", "state_code", "type", "latitude", "longitude"]]
+    states.columns = ["StateID", "StateName", "CountryID", "StateCode", "StateType", "StateLatitude", "StateLongitude"]
+
+    # ? Cities Master Table
