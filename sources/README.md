c7f32b8fb3529367e97ea5cc382b585e599d20f6
E33605 <debmalyapramanik.005@gmail.com>
2024-01-12 12:01:45 +0530

    🎉 create a master tables for global indicator references

--- sources/README.md
+++ sources/README.md
@@ -6,4 +6,8 @@
 
 The directory lists out comprehensive lists of all the data sources, or API informations used for building a 360° global macroeconomics view. Open-source data sources are either referenced, or scripts are defined in case of API sources with a free subscription plan. Proprietary data sources and pricing informations is available in [PROPRIETARY_SOURCES.md](./PROPRIETARY_SOURCES.md) file, along with any other information.
 
+## World Bank & International Monetary Fund (IMF)
+
+A list of globally recognised sourced macro-economic data available from World Bank and IMF that provides detailed indicators like WDI, GDP, PPP, etc. is collected and stored on an anuual granularity. Check the category and sub-category master tables for more information on the indicators and its details.
+
 </div>
---
+++ sources/mwIndicators.sql
@@ -0,0 +1,42 @@
+/********************************************************************
+Master Table Definations, and Table Initialization Statements
+
+Global indicators like WDI, GDP, PPP are sourced from World Bank and
+International Monetary Fund (IMF). This data is stored into database
+and the same is tracked and categorized using the following master
+tables. The category information is modified/changed as required.
+
+Author  : Debmalya Pramanik
+Version : v0.0.1
+
+Copywright © [2024] neuralNOD INC., Debmalya Pramanik
+********************************************************************/
+
+CREATE TABLE [dbo].[mwCategoryIndicator] (
+    CategoryID      VARCHAR(3) PRIMARY KEY,
+    CategoryName    VARCHAR(64) UNIQUE NOT NULL,
+    CategoryDesc    VARCHAR(4096) NOT NULL
+);
+
+INSERT INTO [dbo].[mwCategoryIndicator] (CategoryID, CategoryName, CategoryDesc) VALUES
+    ('WDI', 'World Development Indicators', 'WDI is the primary collection of development indicators, which includes national, regional and global data available. Important indicators like GDP, Mortality Rate, etc. are tracked under this category. The information is sourced from World Bank and IMF, who in-turn source the data from recogniged sources. The data from IMF has a greater precision, however NUMERIC(*, 2) is best for tracking of global indicators. In addition, IMF has a good API facility which provides forecasted result, extrapolated from the current/last available data point. This information is available on a "YEARLY" granularity.'),
+    ('FSI', 'Financial Sector Indicators', 'FSI is the primary collection of financial indicators, like GDF, GFI, debts, budget allocations, etc. on a national level. This information is available on a "YEARLY" granularity.'),
+    ('HNP', 'Health, Nutrition & Population Statistics', 'HNP tracks informations on a national level, about the health, quality of life, population information etc. This information is available on a "YEARLY" granularity.'),
+    ('WGI', 'Worldwide Governance Indicators', 'Aggregate and individual governance indicators for six dimensions of governance: Voice and Accountability; Political Stability and Absence of Violence/Terrorism; Government Effectiveness; Regulatory Quality; Rule of Law; Control of Corruption.');
+
+CREATE TABLE [dbo].[mwSubCategoryIndicator] (
+    SubCategoryID      VARCHAR(3) PRIMARY KEY,
+    CategoryID         VARCHAR(3) NOT NULL,
+    SubCategoryName    VARCHAR(64) UNIQUE NOT NULL,
+    SubCategoryDesc    VARCHAR(4096) NOT NULL,
+
+    CONSTRAINT category_id_fk FOREIGN KEY(CategoryID) REFERENCES mwCategoryIndicator(CategoryID)
+);
+
+INSERT INTO [dbo].[mwSubCategoryIndicator] (SubCategoryID, CategoryID, SubCategoryName, SubCategoryDesc) VALUES
+    ('GDF', 'FSI', 'International Debt Statistics', 'International Debt Statistics (IDS), formerly known as Global Development Finance (GDF) - provides external debt and financial flows statistics for countries that report public and publicly-guaranteed debt under the World Banks Debtor Reporting System (DRS).'),
+    ('GFI', 'FSI', 'Global Financial Inclusions', 'Indicators of financial inclusions, summarized for all adults and disaggregated by key demographic chareteristics like gender, age, education.'),
+    ('ICP', 'WDI', 'International Comparison Program', 'A collection of comparative price data and detailed expenditure values of countries gross domestic products (GDP), and purchasing power parities (PPPs) estimates of the worlds economies.'),
+    ('GEM', 'FSI', 'Global Economic Monitor', 'Updates of global economic developments for high income and developing countries - covers exchange rates, equity markets, interest rates, stripped bond spreads, and emerging market bond indices'),
+    ('FPN', 'HNP', 'Food Prices for Nutrition', 'Food Prices for Nutrition provides indicators on the cost and affordability of healthy diets in each country, showing the populations physical and economic access to sufficient quantities of locally available items for an active and healthy life. It also provides indicators on the cost and affordability of an energy-sufficient diet and a nutrient-adequate diet.'),
+    ('SEA', 'WDI', 'Sustainable Energy for All', 'The “Sustainable Energy for All (SE4ALL)” initiative, launched in 2010 by the UN Secretary General, established three global objectives to be accomplished by 2030: to ensure universal access to modern energy services, to double the global rate of improvement in global energy efficiency, and to double the share of renewable energy in the global energy mix. SE4ALL database supports this initiative and provides country level historical data for access to electricity and non-solid fuel; share of renewable energy in total final energy consumption by technology; and energy intensity rate of improvement.');
